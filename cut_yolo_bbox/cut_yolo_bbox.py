c01408467b3adc6a13b7b392840d7e66c4fb5b14
Волконский Ярослав Иванович <YIVolkonskiy@sberbank.ru>
2022-02-24 18:28:30 +0300

    updt

--- cut_yolo_bbox/cut_yolo_bbox.py
+++ cut_yolo_bbox/cut_yolo_bbox.py
@@ -5,6 +5,14 @@ import textwrap
 import argparse
 
 
+class StoreDictKeyPair(argparse.Action):
+     def __call__(self, parser, namespace, values, option_string=None):
+         my_dict = {}
+         for kv in values.split(","):
+             k,v = kv.split("=")
+             k=int(k)
+             my_dict[k] = v
+         setattr(namespace, self.dest, my_dict)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(prog='Cut bbox yolo',
@@ -14,10 +22,12 @@ if __name__ == '__main__':
     parser.add_argument('-l', '--labels_pth', type=str, required=True)
     parser.add_argument('-i','--imgs_pth', type=str, required=True)
     parser.add_argument('-o', '--out_pth', type=str, required=True) # how often take frame
+    parser.add_argument('-z','--zamena', action=StoreDictKeyPair, metavar='KEY1=VAL1,KEY2=VAL2...') #, dest='my_dict'
 
     args = parser.parse_args()
     print(args)
 
+    zamena=args.zamena
     labels_pth=args.labels_pth
     imgs_pth=args.imgs_pth
     out_pth=args.out_pth
@@ -26,11 +36,14 @@ if __name__ == '__main__':
     labels_pth=glob.glob(f"{labels_pth}*.txt")
 
     for cur_lab in labels_pth: 
+        if cur_lab.endswith("classes.txt"):
+            continue
         name_file=os.path.basename(cur_lab)
+        print(name_file)
         name_file=name_file[:name_file.rfind(".")]
         
         for img_name in os.listdir(imgs_pth):
-            if (img_name).startswith(name_file+"."):
+            if (img_name).startswith(name_file+".") and not (img_name).endswith(name_file+".txt"):
                 break
         
         img = cv2.imread(os.path.join(imgs_pth,img_name))
@@ -38,7 +51,10 @@ if __name__ == '__main__':
         
         f = open(cur_lab,'r')
         f = f.read()
+        if len(f)<3:
+            continue
         f=f.strip().split("\n")
+        
         for idx,i in enumerate(f):
             cl,x1,y1,x2,y2=i.split(" ")
             cl,x1,y1,x2,y2=float(cl),float(x1),float(y1),float(x2),float(y2)
@@ -48,7 +64,7 @@ if __name__ == '__main__':
             ystrt=int((y1-y2/2)*h)
             yend=int((y1+y2/2)*h)
             tmp_img=img[ystrt:yend, xstrt:xend]
-            tmp_outpath=os.path.join(out_pth, name_file+".jpg")
+            tmp_outpath=os.path.join(out_pth, zamena[int(cl)],name_file+".jpg")
             cv2.imwrite(tmp_outpath, tmp_img)
 
 
